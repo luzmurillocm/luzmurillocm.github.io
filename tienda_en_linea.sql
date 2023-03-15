32b41f8aea1b0449cadc952daf1e57614fd81faf
luzmurillocm <124410747+luzmurillocm@users.noreply.github.com>
2023-03-14 18:58:24 -0600

    Add files via upload

---
+++ tienda_en_linea.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-03-2023 a las 01:57:17
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `evaluacion1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tienda en linea`
+--
+
+CREATE TABLE `tienda en linea` (
+  `Instrumento o Accesorio` int(11) NOT NULL,
+  `Acustico o Electrico` int(11) NOT NULL,
+  `Precio` double NOT NULL,
+  `Color` varchar(100) NOT NULL,
+  `Descripcion` varchar(100) NOT NULL,
+  `Descuento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
