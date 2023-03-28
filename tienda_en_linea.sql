b97ff8518b7f994c3cc654e6bd06ce3ff2bab331
luzmurillocm <124410747+luzmurillocm@users.noreply.github.com>
2023-03-27 19:01:51 -0600

    Practica 13 continuacion
    continuación

--- tienda_en_linea.sql
+++ tienda_en_linea.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-03-2023 a las 01:57:17
--- Versión del servidor: 10.1.39-MariaDB
--- Versión de PHP: 7.3.5
+-- Tiempo de generación: 28-03-2023 a las 03:01:00
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,23 +18,113 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `evaluacion1`
+-- Base de datos: `tienda_en_linea`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tienda en linea`
+-- Estructura de tabla para la tabla `categorias`
 --
 
-CREATE TABLE `tienda en linea` (
-  `Instrumento o Accesorio` int(11) NOT NULL,
-  `Acustico o Electrico` int(11) NOT NULL,
-  `Precio` double NOT NULL,
-  `Color` varchar(100) NOT NULL,
-  `Descripcion` varchar(100) NOT NULL,
-  `Descuento` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'mesa de noche', 'Diseño simple de base de metal: el elegante juego de 2 piezas utiliza una base de metal única como cuerpo de la lámpara para que coincida con la visualización de tela amarilla clara para crear un esti'),
+(2, 'Accesorios de Baño', 'Material resistente y duradero: este juego de baño está hecho de material de acero inoxidable de alta calidad, que no es fácil de corroer ni oxidar, lo suficientemente resistente como para soportar ca'),
+(3, 'Lámpara de mesa inteligente', 'Pulsa la parte superior para encender/apagar: el creativo sensor táctil integrado te permite tocar dos veces en la parte superior de la lámpara para encenderla o apagarla. También puedes trabajar manu'),
+(4, 'Silla Gamer', 'Muy conveniente\r\nInclinación ajustable: ideal en esas partidas relajadas y para aquellas en las que es necesario jugar en serio. Hasta 155° de inclinación y altura adaptable\r\nComodidad durante tus par'),
+(5, 'Silla giratoria para ​niños', '360° pulido liso integral : moldeado de laminación, disco grueso, diseño científico, asiento en forma de huevo, convexo en ambos lados, diseño cóncavo en el centro, fácil de colocar.\r\nEntrenamiento de');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int(11) NOT NULL,
+  `total` float NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta_productos`
+--
+
+CREATE TABLE `venta_productos` (
+  `id` int(11) NOT NULL,
+  `venta_id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `venta_productos`
+--
+ALTER TABLE `venta_productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `venta_id` (`venta_id`),
+  ADD KEY `producto_id` (`producto_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta_productos`
+--
+ALTER TABLE `venta_productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `venta_productos`
+--
+ALTER TABLE `venta_productos`
+  ADD CONSTRAINT `venta_productos_ibfk_1` FOREIGN KEY (`venta_id`) REFERENCES `ventas` (`id`),
+  ADD CONSTRAINT `venta_productos_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `categorias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
